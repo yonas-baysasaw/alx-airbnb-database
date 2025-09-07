9c9f4f7ebd432927b1e4c4c32f998eca419ea1d6
Yonas Baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-07 17:52:16 +0300

    Practice Subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,9 +1,9 @@
-SELECT *
-FROM Properties p
-WHERE p.property_id IN (
-    SELECT r.property_id
-    FROM Reviews r
-    GROUP BY r.property_id
-    HAVING AVG(r.rating) > 4.0
-)
-ORDER BY p.property_id;
+-- Correlated subquery: Users who have made more than 3 bookings
+SELECT u.user_id, u.name, u.email
+FROM Users u
+WHERE (
+    SELECT COUNT(*)
+    FROM Bookings b
+    WHERE b.user_id = u.user_id
+) > 3
+ORDER BY u.user_id;
