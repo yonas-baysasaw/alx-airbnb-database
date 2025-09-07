c79c2f96fc3562269b6bf58c97b02b12364ba0d3
Yonas Baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-09-07 17:46:16 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM Properties p
+WHERE p.property_id IN (
+    SELECT r.property_id
+    FROM Reviews r
+    GROUP BY r.property_id
+    HAVING AVG(r.rating) > 4.0
+)
+ORDER BY p.property_id;
